326e50c61791c28f802e9812c80f82c48a081285
SvetoslavaNikolova13 <svetleto31@gmail.bg>
2023-05-24 23:02:47 +0100

    upload projects

--- Nesebar website/Black sea.js
+++ Nesebar website/Black sea.js
@@ -2,19 +2,19 @@
 window.addEventListener('load', () =>{"all your code"}) 
 //Get the first element of array button and array navbar-links
 
-/*const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
- toggleButton.addEventListener('click',() => {
+ toggleButton.addEventListener('click',  function() {
     navbarLinks.classList.toggle('active')
- })*/
+ })
 
 
- function myFunction() {
+ /*(function myFunction() {
     const navbarLinks = document.getElementsByClassName('navbar-links')[0]
     if (navbarLinks.style.display == "block") {
         navbarLinks.style.display = "none";
     } else {
         navbarLinks.style.display = "block";
     }
-  }
+  }*/
  
